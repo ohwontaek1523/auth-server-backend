b0da081d3585fa68e5beec4f1bc3cac390d84123
oh wontaek <oh.wt@astkorea.net>
2025-11-10 10:12:54 +0900

    git-test 수정

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
-
+//git-test 수정사항 : login.dto.ts
 export class LoginDto {
   @ApiProperty({
     description: '이메일 주소',
